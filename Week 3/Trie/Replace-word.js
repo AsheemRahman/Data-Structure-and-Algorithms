d05d8d236ab0ceba59cad1b4e5543ea6ff3ce0b7
Asheem Rahman <mhdasheem@gmail.com>
2024-09-26 11:03:47 +0530

    Currect Question

--- Week 3/Trie/Replace-word.js
+++ Week 3/Trie/Replace-word.js
@@ -21,6 +21,41 @@ class Trie {
         curr.endOfWord = true
     }
 
+    search(word) {
+        let node = this.root
+        for (let char of word) {
+            if (!node.children[char]) {
+                return false
+            }
+            node = node.children[char]
+        }
+        return true
+    }
+
+    delete(word) {
+        this.deleteHelper(this.root, word, 0)
+    }
+
+    deleteHelper(node, word, index) {
+        if (index === word.length) {
+            if (!node.endOfWord) {
+                return false
+            }
+            node.endOfWord = false
+            return Object.keys(node.children).length === 0
+        }
+        let char = word[index]
+        if (!node.children[char]) {
+            return false
+        }
+        let deleteChild = this.deleteHelper(node.children[char], word, index + 1)
+        if (deleteChild) {
+            delete node.children[char]
+            return Object.keys(node.children).length === 0 && !node.endOfWord
+        }
+        return false
+    }
+
     // replace the word in trie
 
     replace(oldWord, newWord) {
