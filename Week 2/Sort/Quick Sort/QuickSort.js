8c4d0e4058a4676ca2143b317b8383d8926cdd7c
Asheem Rahman <mhdasheem@gmail.com>
2025-01-26 14:29:39 +0530

    Fix QuickSort Question

--- Week 2/Sort/Quick Sort/QuickSort.js
+++ Week 2/Sort/Quick Sort/QuickSort.js
@@ -1,20 +1,20 @@
-function bubbleSort(arr) {
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[j] = temp;
-                swapped = true;
-            }
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
         }
-    } while (swapped);
-    
-    return arr
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
 const arr = [85, 746, 52, 55, -82, 2, -566]
 
-console.log(bubbleSort(arr)) // output  : [-566,-82,2,52,55,85,746]
+console.log(quickSort(arr)) // output  : [-566,-82,2,52,55,85,746]
